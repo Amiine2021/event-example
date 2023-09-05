819ff699593303fe69c594edec6301784f7fb2d4
amine <mraminewiebke@gmail.com>
2023-09-05 12:36:38 +0200

    event test

---
+++ main.js
@@ -0,0 +1,22 @@
+let inp = document.getElementById('inp');
+// let i = 1;
+
+// inp.onkeyup = function (){
+   
+//     console.log('click' + i++)
+// }
+// onfocus 
+// inp.onfocus = () =>{
+//     inp.style.border = '10px solid red'
+// }
+// // onblur 
+// inp.onblur = () =>{
+//     inp.style.border = 'none'
+// }
+
+// onload = function(){
+//     document.body.style.background = 'yellow'
+// }
+onresize = function(){
+    document.body.style.background = 'yellow'
+}
